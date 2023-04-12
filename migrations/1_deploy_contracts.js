3a48eb6528970aa0d20719623b83c091b8847fc9
WenqingZhong <zhongwenqing99@gmail.com>
2023-04-12 11:02:52 -0400

    minor changes in migration

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,8 +1,8 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+
+const Market= artifacts.require("Market");
+const NFT = artifacts.require("NFT");
 
 module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+  deployer.deploy(Market);
+  deployer.deploy(NFT);
 };
