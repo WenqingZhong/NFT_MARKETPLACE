70f0898bb84534090f655caf5ec24eaeb01c443e
WenqingZhong <zhongwenqing99@gmail.com>
2023-04-12 11:22:25 -0400

    fist test

--- test/Market.js
+++ test/Market.js
@@ -1,5 +1,6 @@
 const Market= artifacts.require("Market");
 const NFT= artifacts.require("NFT");
+const {expectRevert}= require("@openzeppelin/test-helpers");
 
 contract ('Market',()=>{
     let market;
@@ -7,9 +8,22 @@ contract ('Market',()=>{
     before (async ()=> {
         market= await Market.deployed();
         token = await NFT.deployed();
+
+        await token.mint();
     });
 
-    describe("List Token", ()=> {
+    const tokenId = 1;
+    const price = 1000;
 
+    describe("List Token", ()=> {
+        it ('should not listing if contract is not approved', ()=> {
+            return expectRevert(
+                market.listToken(
+                token.address,
+                tokenId,
+                price
+                ), 'ERC721: caller is not token owner or approved' 
+            );
+        })
     });
 });
